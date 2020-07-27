6ca4f5e4a13668220fdafa1ad9c8b4b30729eb29
Tapan Khandelwal <38181043+Juggernaut1997@users.noreply.github.com>
2020-07-27 10:00:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from flask import Flask,request, url_for, redirect, render_template, jsonify
 from pycaret.regression import *
 import pandas as pd
@@ -15,11 +18,7 @@ def home():
 @app.route('/model/<name>',methods=['POST'])
 def model(name):
     global model, cols
-#     model = load_model(name)
-    if name=="infy_bank":
-        cols = ['Age', 'Experience', 'Income', 'ZIP Code', 'Family', 'CCAvg', 'Education', 'Mortgage', 'Securities Account', 'CD Account', 'Online', 'CreditCard']
-    else:
-        cols = ['satisfaction_level', 'last_evaluation', 'number_project', 'average_montly_hours', 'time_spend_company', 'Work_accident', 'promotion_last_5years', 'Department', 'salary']
+    cols = ['cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment','gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring','stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type','veil-color','ring-number','ring-type','spore-print-color','population','habitat']
     file = name+".html"
     return render_template(file)
 
@@ -28,16 +27,16 @@ def predict(name):
     global model
     int_features = [x for x in request.form.values()]
     final = np.array(int_features)
-    model = load_model(name)
+    name1 = name+"_training_pipeline"
+    model = load_model(name1)
     data_unseen = pd.DataFrame([final], columns = cols)
-    if name=='churn':
-        prediction = predict_model(model, data=data_unseen, round = 0)
-        prediction = int(prediction.Label[0])
+    prediction = model.predict(data_unseen)
+    if int(prediction)==1:
+        pred="Poisonous"
     else:
-        prediction = model.predict(data_unseen)
-        prediction = int(prediction)
+        pred="Edible"
     file = name+".html"
-    return render_template(file,pred='The chance of this person is {}'.format(prediction))
+    return render_template(file,pred='The mushroom is {}'.format(pred))
 
 if __name__ == '__main__':
     app.run()
